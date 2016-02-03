cf8a7e78cff45c6c9b5240e6f849781ea7290968
Sofia Johansson <sj222mk@student.lnu.se>
2016-02-03 11:40:55 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   get 'users/new'
   
   get 'signup'  => 'users#new'
+  
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+root 'users#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,13 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
-    assert_select "title", "NewUser | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Sign up| Webbramverk"
+  end
+  
+  test "should get show" do
+    get :show
+    assert_response :success
+    assert_select "title", "@user.name | Webbramverk"
   end
 
 end
