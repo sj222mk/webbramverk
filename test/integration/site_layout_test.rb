782645a160086e42701ce26be8ed1a70d7d492ab
Sofia Johansson <sj222mk@student.lnu.se>
2016-02-24 20:26:20 +0000

    some fix

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,7 +5,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     # assert_template 'application_controller/home'
-    assert_select "a[href=?]", root_path, count: 0
+    assert_select "a[href=?]", root_path, count: 3
     get signup_path
     # assert_select "title", full_title("Sign up")
   end
