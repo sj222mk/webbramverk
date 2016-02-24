8a600c1aff125ecbdaa63d68e70313ab954d6ff9
Sofia Johansson <sj222mk@student.lnu.se>
2016-02-24 22:20:39 +0000

    seeds fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create!(name:  "Admin User",
              email: "admin@email.com",
-             password:              "adminpassword",
-             password_confirmation: "adminpassword",
+             password:              "password",
+             password_confirmation: "password",
              admin: true)
 
 10.times do |n|
