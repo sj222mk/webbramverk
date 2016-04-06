22bf4efddeb3177a228db12d422579423ebca56d
Sofia Johansson <sj222mk@student.lnu.se>
2016-04-06 19:12:20 +0000

    redirect fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   
@@ -64,7 +64,6 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
-    
     
     # Confirms the correct user.
     def correct_user
